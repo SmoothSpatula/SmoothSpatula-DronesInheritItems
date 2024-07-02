276448841d5554cb0c32bc1672e873ef9f49b535
Gabriel Combe-Ounkham <gabrielcombe@gmail.com>
2024-07-02 20:17:56 +0100

    Update main.lua, fix items not working

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
--- Drones Inherit Items v1.0.0
+-- Drones Inherit Items v1.0.1
 -- SmoothSpatula
 
 log.info("Successfully loaded ".._ENV["!guid"]..".")
@@ -20,21 +20,32 @@ gui.add_to_menu_bar(function()
         Toml.save_cfg(_ENV["!guid"], params)
     end
 end)
+
 -- ========== Main ==========
 
 gm.post_script_hook(gm.constants.item_give_internal, function(self, other, result, args)
     if not params['drones_inherit_items_enabled'] then return end
 
+    local actor = args[1].value
+    local item_id = args[2].value
+    local count = args[3].value
+    local stack_kind = args[4].value
+
+    if actor.object_index ~= gm.constants.oP then return end
+
     local drones = Helper.find_active_instance_all(gm.constants.pDrone)
     for _, drone in ipairs(drones) do
-        drone:recalculate_stats()
+        gm.item_give(drone, item_id, count, stack_kind)
     end
 end)
 
 gm.post_script_hook(gm.constants.init_drone, function(self, other, result, args)
     if not params['drones_inherit_items_enabled'] then return end
 
-    self.inventory_item_order = self.master.inventory_item_order
-    self.inventory_item_stack = self.master.inventory_item_stack
-    self:recalculate_stats()
+    local inventory_order = self.master.inventory_item_order
+    local inventory_stack = self.master.inventory_item_stack
+
+    for _, item_id in ipairs(inventory_order) do
+        gm.item_give(self, item_id, inventory_stack[item_id+1], 1)
+    end
 end)
